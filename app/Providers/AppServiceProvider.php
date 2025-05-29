99743255c73b9324844c41780d3f7e23d3009179
kevin-1111102 <97698002+Kevin-1111102@users.noreply.github.com>
2025-05-29 23:06:22 +0200

    removed binded code in service provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,27 +3,11 @@
 namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Support\Facades\Route;
-use App\Models\Adres;
-use Illuminate\Database\Eloquent\ModelNotFoundException;
 
 class RouteServiceProvider extends ServiceProvider
 {
-    public function boot()
+    public function boot(): void
     {
         parent::boot();
-
-        Route::bind('adres', function ($value) {
-            $adres = Adres::where('id', $value)
-                ->where('user_id', auth()->id())
-                ->first();
-
-            if (!$adres) {
-                throw (new ModelNotFoundException)->setModel(Adres::class);
-            }
-
-            return $adres;
-        });
     }
-
 }
